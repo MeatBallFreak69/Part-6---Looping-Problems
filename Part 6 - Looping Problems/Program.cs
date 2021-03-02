b6eab9b607330931e4e1288e2f1af20550cee493
Clark Miller <glitchclick@gmail.com>
2021-03-02 11:14:34 -0500

    Added first program

--- Part 6 - Looping Problems/Program.cs
+++ Part 6 - Looping Problems/Program.cs
@@ -10,6 +10,35 @@ namespace Part_6___Looping_Problems
     {
         static void Main(string[] args)
         {
+            int minValue;
+            int maxValue;
+            int numBetween;
+            Console.WriteLine("Please enter 2 numbers, first being a minimum and second a maximum value");
+            Console.WriteLine("Minimum");
+
+            while (!Int32.TryParse(Console.ReadLine(), out minValue))
+                Console.WriteLine("Invalid integer! Try again");
+
+            Console.WriteLine("Maximum");
+
+            while (!Int32.TryParse(Console.ReadLine(), out maxValue))
+                Console.WriteLine("Invalid integer! Try again");
+
+            Console.WriteLine($"Now enter a number between {minValue} and {maxValue}");
+
+            do
+            {
+                while (!Int32.TryParse(Console.ReadLine(), out numBetween))
+                    Console.WriteLine("Invalid integer! Try again");
+                if (numBetween < minValue || numBetween > maxValue)
+                {
+                    Console.WriteLine("Error! Incorrect value entered");
+                }
+            } while (numBetween < minValue || numBetween > maxValue);
+            Console.WriteLine($"Your number which is {numBetween} is in the range of {minValue} and {maxValue}");
+
+            Console.ReadLine();
         }
     }
 }
+
